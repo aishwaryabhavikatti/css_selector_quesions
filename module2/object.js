bdbc63dd3f3013103409085f3c77a4b98d0dece9
aishwaryabhavikatti <aishusb210@gmail.com>
2022-03-08 22:17:08 +0530

    objects

---
+++ module2/object.js
@@ -0,0 +1,25 @@
+// let obj={};
+// console.log(obj);
+
+// let objAishu={
+//     firstname: "Aishu",
+//     age: 23,
+//     phoneNo:8792583054,
+//     lastname:"bhavikatti"
+// }
+// console.log(objAishu);
+
+
+let capAmerica={
+    Name:'Steves',
+    age:43,
+    friends:['natasha', 'thor', 'tony'],
+    address:{ 
+        city:'queens',
+        state:'haryana'
+    },
+    sayHi:function(){
+        console.log("cap america says hi..")
+    }
+}
+console.log(capAmerica);
