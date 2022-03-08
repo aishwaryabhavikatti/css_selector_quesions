76165b5d21d96d856497449fe5cb6fe12050256a
aishwaryabhavikatti <aishusb210@gmail.com>
2022-03-09 00:14:27 +0530

    update

--- module2/node/childProcess.js
+++ module2/node/childProcess.js
@@ -1,2 +1,7 @@
-let cp=require('child_process');
-cp.execFileSync("gnome-calc");
+let cp =  require('child_process');
+// console.log(cp);
+
+//open calculator with the help of this file
+
+// for windows replace gnome-calculator with calc
+cp.execFileSync("calc");
