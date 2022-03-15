6352ba1c154cc60334415d7c30c77bc6ab408b32
aishwaryabhavikatti <aishusb210@gmail.com>
2022-03-15 18:30:08 +0530

    crud

--- module2/node/fs.js
+++ module2/node/fs.js
@@ -8,3 +8,16 @@ console.log(filePath);
 
 //create file if it does not exists else it oerride
 fs.writeFileSync(filePath,"hello i am txt file");
+
+//read
+console.log('before update')
+let content = fs.readFileSync(filePath,'utf-8');
+console.log(content)
+
+//update
+fs.appendFileSync(filePath,'newly added')
+console.log("after update")
+console.log(fs.readFileSync(filePath,'utf-8'));
+
+//delete
+fs.unlinkSync(filePath)
