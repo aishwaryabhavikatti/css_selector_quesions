3471bbee6ac7e681c9b46525d53405de1a6ceb98
aishwaryabhavikatti <aishusb210@gmail.com>
2022-03-15 19:40:35 +0530

    crud directory

--- module2/node/fs.js
+++ module2/node/fs.js
@@ -1,23 +1,37 @@
 let fs=require("fs");
 //console.log(fs);
 
-let path= require("path");
+// let path= require("path");
 
-let filePath=path.join(__dirname,"file.txt");
-console.log(filePath);
+// let filePath=path.join(__dirname,"file.txt");
+// console.log(filePath);
 
-//create file if it does not exists else it oerride
-fs.writeFileSync(filePath,"hello i am txt file");
+// //create file if it does not exists else it oerride
+// fs.writeFileSync(filePath,"hello i am txt file");
 
-//read
-console.log('before update')
-let content = fs.readFileSync(filePath,'utf-8');
-console.log(content)
+// //read
+// console.log('before update')
+// let content = fs.readFileSync(filePath,'utf-8');
+// console.log(content)
 
-//update
-fs.appendFileSync(filePath,'newly added')
-console.log("after update")
-console.log(fs.readFileSync(filePath,'utf-8'));
+// //update
+// fs.appendFileSync(filePath,'newly added')
+// console.log("after update")
+// console.log(fs.readFileSync(filePath,'utf-8'));
 
-//delete
-fs.unlinkSync(filePath)
+// //delete
+// fs.unlinkSync(filePath)
+
+//create directory
+
+if(!fs.existsSync("hamariDirectory"))
+fs.mkdirSync("hamariDirectory");
+
+//read directory
+let folderPath=__dirname;
+let contentOfFolder= fs.readdirSync(folderPath);
+console.log(contentOfFolder);
+
+
+//delete 
+fs.rmdirSync("hamariDirectory");
