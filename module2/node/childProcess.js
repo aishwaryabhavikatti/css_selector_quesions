b8d3c4d2da3d8feb5cb108b79007f6b5b5a240ca
aishwaryabhavikatti <aishusb210@gmail.com>
2022-03-15 17:15:59 +0530

    child process

--- module2/node/childProcess.js
+++ module2/node/childProcess.js
@@ -4,4 +4,10 @@ let cp =  require('child_process');
 //open calculator with the help of this file
 
 // for windows replace gnome-calculator with calc
-cp.execFileSync("calc");
+// cp.execFileSync("calc");
+//cp.execFileSync("code");
+
+let content=cp.execSync("node test.js");
+//console.log("output of test.js file: "+content)
+//console.log(content)
+console.log(""+content)
